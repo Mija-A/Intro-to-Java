1e46b4422d1c7edd18abecbd064cb82cac0aa18a
Mija-A <156041545+Mija-A@users.noreply.github.com>
2024-01-22 20:26:31 -0500

    Create Nested for loop.java

---
+++ Nested for loop.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {		
+		String jolly = "*";
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.println(i);
+				
+			}
+			System.out.println("");
+			
+		}
+	}	
+}
+
+
+// 0
+
+// 1
+// 1
+
+// 2
+// 2
+// 2
+
+// 3
+// 3
+// 3
+// 3
+
+// 4
+// 4
+// 4
+// 4
+// 4
+
+// 5
+// 5
+// 5
+// 5
+// 5
+// 5
+
+
