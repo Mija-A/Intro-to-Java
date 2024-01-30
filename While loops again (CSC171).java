6eafcfb502af0564dd3ffb0c34d6cbd2ae15838a
Mija-A <156041545+Mija-A@users.noreply.github.com>
2024-01-30 14:16:00 -0500

    Create While loops again (CSC171).java

---
+++ While loops again (CSC171).java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SayHello {
+	public static void main(String[] args) {				
+		
+		int x = 1;
+		
+		while (x <= 5) {
+			System.out.println(7 * x);
+			
+			x = x +1;
+			
